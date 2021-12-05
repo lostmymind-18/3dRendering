826ddf0dc546e878994fe1addffea999d9a32ad1
Trần Mạnh Dũng <78700107+lostmymind-18@users.noreply.github.com>
2021-12-05 12:06:42 +0700

    Update 3D.cpp

--- 3D.cpp
+++ 3D.cpp
@@ -5,20 +5,63 @@
 #include<unistd.h>
 using namespace std;
 
-#define HEIGHT 80
-#define WIDTH 80
+#define pi 3.14159265358979323846
 
-struct Point{
-    int x,y,z;
+
+int WIDTH = 120, HEIGHT = 60;
+
+
+struct Point
+{
+    int x, y, z;
+};
+
+struct Cube{
+    vector<Point> Points;
+    Cube()
+    {
+        Points.push_back({55,55,75});
+        Points.push_back({55,55,5});
+        Points.push_back({55,5,75});
+        Points.push_back({55,5,5});
+        Points.push_back({5,55,75});
+        Points.push_back({5,55,5});
+        Points.push_back({5,5,75});
+        Points.push_back({5,5,5});
+    }
+    void project(float a)
+    {
+        for(auto p:this->Points)
+        {
+            p.x = p.x + p.z*tan(a/2);
+            p.y = p.y - p.z*tan(a/2);
+        }
+    }
 };
 
 int main()
 {
-    Point A[8] = {{10,10,10},{-10,10,10},{-10,-10,10},{10,-10,10},
-                {10,10,20},{-10,10,20},{-10,-10,20},{10,-10,20}        
-    };
-    for(int i = 0; i < HEIGHT)
+    Cube cube;
+    float a = pi;
+    cube.project(a);
+    for(auto p:cube.Points)
+        cout<<p.x<<"    "<<p.y<<"   "<<p.z<<endl;
+    for(int i = 0; i < HEIGHT; i++)
     {
-        for(int j = 0; j < )
+        for(int j = 0; j < WIDTH; j++)
+        {
+            bool a =false;
+            for(auto point:cube.Points)
+            {
+                if(i == point.y  && j == point.x )
+                {
+                    cout<<"#";
+                    a = true;
+                } 
+            }
+            if(!a)
+                cout<<" ";
+        }
+        cout<<endl;
     }
 }
